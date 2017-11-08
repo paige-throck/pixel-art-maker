166f38a81e8f1d6e8aa6bb3d838349669bbb56ba
Paige Throckmorton <path4942@gmail.com>
2017-11-08 10:07:45 -0600

    Pixel Art MAker one complete, creating Pixel Art maker 2 dynamically

---
+++ script.js
@@ -0,0 +1,31 @@
+let penColour = 'black';
+
+function setPenColour(pen) {
+  penColour = pen;
+  let currentColourPen = document.getElementById('currentPen');
+  currentColourPen.style.backgroundColor = penColour;
+}
+
+let pixelGrid = document.getElementById('pixelGrid');
+pixelGrid.addEventListener('click', function() {
+  event.target.style.backgroundColor = penColour;
+
+});
+
+
+function clearPixelGrid() {
+  let pixels = document.getElementsByClassName('pixel');
+  console.log("Pixels foo", pixels);
+  Array.from(pixels).forEach(function(pixel) {
+    pixel.style.backgroundColor = 'white';
+  });
+  // for (var i = 0; i < pixels.length; i++) {
+  //   let pixel = pixels[i];
+  //   pixel.style.backgroundColor = 'white';
+  // }
+}
+
+let clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', function() {
+  clearPixelGrid();
+});
