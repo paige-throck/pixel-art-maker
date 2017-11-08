9a42cbea4ab1d52d1523efda70f037bbe1fa6037
Paige Throckmorton <path4942@gmail.com>
2017-11-08 12:56:24 -0600

    Created 90's style pix-grid, pix-1 and created dynamic grid pix-2

--- pixel-art-maker-2/script.js
+++ pixel-art-maker-2/script.js
@@ -1,39 +1,41 @@
 let pixelGrid = document.getElementById('pixelGrid');
 
-for (i=0; i < 3000; i++){
+
+for (i=0; i < 2500; i++){
   let pixel = document.createElement('div');
   pixel.classList.add('pixel');
   pixelGrid.appendChild(pixel);
 }
 
-// let penColour = 'black';
-//
-// function setPenColour(pen) {
-//   penColour = pen;
-//   let currentColourPen = document.getElementById('currentPen');
-//   currentColourPen.style.backgroundColor = penColour;
-// }
-//
-// let pixelGrid = document.getElementById('pixelGrid');
-// pixelGrid.addEventListener('click', function() {
-//   event.target.style.backgroundColor = penColour;
-//
-// });
-//
-// 
-// function clearPixelGrid() {
-//   let pixels = document.getElementsByClassName('pixel');
-//   console.log("Pixels foo", pixels);
-//   Array.from(pixels).forEach(function(pixel) {
-//     pixel.style.backgroundColor = 'white';
-//   });
-//   // for (var i = 0; i < pixels.length; i++) {
-//   //   let pixel = pixels[i];
-//   //   pixel.style.backgroundColor = 'white';
-//   // }
-// }
-//
-// let clearButton = document.getElementById('clearButton');
-// clearButton.addEventListener('click', function() {
-//   clearPixelGrid();
-// });
+
+let penColour = 'black';
+function setPenColour(pen) {
+  penColour = pen;
+  let currentColourPen = document.getElementById('currentPen');
+  currentColourPen.style.backgroundColor = penColour;
+  }
+
+
+
+pixelGrid.addEventListener('click', function() {
+  event.target.style.backgroundColor = penColour;
+
+});
+
+
+function clearPixelGrid() {
+  let pixels = document.getElementsByClassName('pixel');
+  console.log("Pixels foo", pixels);
+  Array.from(pixels).forEach(function(pixel) {
+    pixel.style.backgroundColor = 'white';
+  });
+  // for (var i = 0; i < pixels.length; i++) {
+  //   let pixel = pixels[i];
+  //   pixel.style.backgroundColor = 'white';
+  // }
+}
+
+let clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', function() {
+  clearPixelGrid();
+});
